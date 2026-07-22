bdd3de99816058bf5ed70ee62ee6daa1637bba0f
harishpalji217 <harishpalji217@gmail.com>
2026-07-22 14:30:42 +0530

    Reduce VUs to 300 for Tor-friendly sustained load

--- 3-stress-test.js
+++ 3-stress-test.js
@@ -9,9 +9,9 @@ export const options = {
       executor: 'ramping-vus',
       startVUs: 0,
       stages: [
-        { duration: '10s', target: 400 },
-        { duration: '30s', target: 1000 },
-        { duration: '876000h', target: 1000 },
+        { duration: '10s', target: 200 },
+        { duration: '20s', target: 300 },
+        { duration: '876000h', target: 300 },
       ],
       gracefulStop: '0s',
       gracefulRampDown: '0s',
